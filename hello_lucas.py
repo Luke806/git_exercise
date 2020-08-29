d4c638d3d689a279c2761e709faee8ad4eee70fc
Luke806 <erinpyo1@gmail.com>
2020-08-29 12:21:01 +0800

    print_goto_function

--- hello_lucas.py
+++ hello_lucas.py
@@ -1,13 +1,19 @@
 print('hello lucas~')
 import turtle
+def print_goto(p_x,p_y):
+    turtle.penup()
+    turtle.goto(p_x,p_y)
+    turtle.pendown()
+
+
 def print_square():
     for i in range(4):
         turtle.forward(100)
         turtle.left(90)
 print_square()
-
+print_goto(120,0)
 def print_triangle():
     for i in range(3):
         turtle.forward(100)
         turtle.left(120)
-
+print_triangle()

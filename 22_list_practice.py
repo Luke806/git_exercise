63e48a092de1d9e496e738e672fe50af1e2e9761
Luke806 <erinpyo1@gmail.com>
2020-09-26 09:32:14 +0800

    22_list_pratice

---
+++ 22_list_practice.py
@@ -0,0 +1,13 @@
+l1 = [9,12,10,11,13]
+print(l1)
+
+l2 = [123,87,56,12]
+l2 = sorted(l2,reverse=True)
+print(l2)
+
+l3 = [123476,163533,27364,374646,73534253748]
+l3 = sorted(l3)
+print(l3)
+
+print('min',l3[0])
+print('max',l3[-1])
